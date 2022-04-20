252c1237d2852c95a2e3fd57b3b517a1703b1a24
Michael Schroeder <magicalmikely@gmail.com>
2022-04-20 15:03:20 -0700

    working through conditional rendering section, part d

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,27 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const resetToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne}
-      text='add by one'
-      />
-      <Button onClick={decreaseByOne}
-      text='subtract by one'
-      />
-      <Button onClick={resetToZero}
-      text='reset'
-      />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
